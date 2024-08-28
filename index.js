b7141242a6871f4c8cb9c58db89dc9418f16cf7f
alexandrolastra <alexandrolastra.fr@gmail.com>
2024-08-29 00:20:35 +0200

    FT_RMT_082024 Alexandro & Kevin

--- index.js
+++ index.js
@@ -34,33 +34,56 @@ if(hackerLength1 < hackerLength2 ) {
     console.log(`The navigator has the longest name, it has ${hackerLength2} characters.`)
 }
 else {
-    console.log(`Wow, you both have equally long names, ${hackerLength1} characters` )
+    console.log(`Wow, you both have equally long names, with ${hackerLength1} characters.` )
 }
 
-////### Iteration 3: Loops
-//const hacker1 = 'Kevin ' 
-//const hacker2 = 'Alexandro'
-
-//  3.1 Print the characters of the driver's name, separated by space, and [in capital letters](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toUpperCase), i.e., `"J O H N"`.
-
-  //3.2 Print all the characters of the navigator's name in reverse order, i.e., `"nhoJ"`.
-
-
-  //3.3 Depending on the [lexicographic order](https://en.wikipedia.org/wiki/Lexicographical_order) of the strings, print: <br>
-
--// `The driver's name goes first.` <br>
--//`Yo, the navigator goes first, definitely.` <br>
--//`What?! You both have the same name?`
 
 
 
-//### Bonus 1:
 
-//Go to the [lorem ipsum generator](http://www.lipsum.com/) website and:
+for (let i < 0; i < hackerLength1-1;i++) {
+  if(hacker1 > hacker2 ) {
+    console.log(`The driver's name goes first.`)
+}
+if(hacker1 < hacker2 ) {
+    console.log(`Yo, the navigator goes first, definitely.`)
+}
+else {
+    console.log(`What?! You both have the same name?` )
+}
+}
 
-  //- Generate 3 paragraphs. Store the text in a new string variable named `longText`.
-  //- Make your program count the number of words in the string.
-  //- Make your program count the number of times the Latin word [`et`](https://en.wiktionary.org/wiki/et#Latin) appears.
+
+let formattedName = ""
+let reverseName = ""
+for (let i=0; i < hackerLength1; i++) {
+    formattedName += hacker1[i].toUpperCase();
+  if (i < hackerLength1 - 1) {
+    formattedName += ' ';
+  }
+}
+console.log(formattedName)
+
+for (let i = hackerLength1; i > 0; i--) {
+    reverseName += hacker1[i - 1]
+}
+console.log(reverseName)
+
+
+
+for(let i = 0; i < 1; i++){
+    if (hacker1 < hacker2){
+        console.log("The driver's name goes first.")
+
+    } else if (hacker1 > hacker2){
+        console.log("Yo, the navigator goes first, definitely.")
+    } else {
+        console.log("What?! You both have the same name?")
+    }
+    }
+
+
+
   const longText= "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent consectetur molestie velit sollicitudin sollicitudin. Nunc convallis lectus vitae purus iaculis, ac dapibus ante egestas. Mauris euismod metus neque, ac venenatis turpis consequat nec. Ut quis aliquam justo, posuere tempor lorem. Praesent eget consequat urna. Duis eleifend nisl cursus velit venenatis auctor. Etiam non porttitor felis. Sed faucibus diam sed augue tristique, id volutpat tortor porta. Etiam at ex non urna dictum cursus. Quisque porttitor efficitur augue eget cursus. Aliquam sit amet ante eu lectus elementum placerat. In sit amet risus fringilla, dapibus odio eget, sollicitudin lectus. Donec lorem lectus, volutpat sed ipsum in, ultricies mollis risus. Sed fringilla ultricies sagittis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam pellentesque varius urna, sit amet malesuada dui sollicitudin id. Aenean malesuada mollis tempor. Vivamus congue aliquet ligula id pretium. Mauris placerat convallis purus id dignissim. Phasellus sed odio laoreet, bibendum felis bibendum, ullamcorper elit. Proin lobortis vulputate porta. Donec eu molestie tortor, vel eleifend libero. Mauris cursus fermentum ex pulvinar volutpat. Suspendisse orci risus, interdum nec lacinia et, imperdiet a nibh. Pellentesque libero nisl, cursus non maximus id, varius eu ex. In ut lacus mattis massa sodales congue. Etiam suscipit quam vel dolor gravida ultricies. Proin tellus orci, consequat a nulla id, viverra convallis erat. Donec erat elit, convallis ut ornare ac, pellentesque vel leo. Cras a imperdiet erat. Proin lacinia nibh id erat elementum, et cursus odio convallis. Aliquam in nisl et turpis consectetur imperdiet. Vestibulum eget aliquam tellus, eget interdum nunc. Praesent vitae tempus diam. Curabitur interdum ullamcorper mi, non imperdiet elit tincidunt eget.";
 
   let numberOfWords= 0;
@@ -71,8 +94,7 @@ else {
   console.log(numberOfWords + 1)
   
    
-  //const resultLongtext = longText.indexOf(" et ")
-  //console.log(resultLongtext) 
+
   
   let countEt= 0;
   for (i =0; i< longText.length; i++) {
@@ -81,9 +103,14 @@ else {
   console.log(countEt)
 
 
-
   //#### Bonus 2:
-
+  let reverse = "";
+  let phraseCheck = "A man a plan a canal Panama"
+  phraseCheck = phraseCheck.toLowerCase().replace(/\s+/g, '');
+  for ( let i = phraseCheck.length - 1; i >= 0; i--){
+    reverse += phraseCheck[i];
+  }
+  console.log(reverse === phraseCheck)
 
 
 
